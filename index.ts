26d6b64ff4538105e4bdd408837567412133cdf3
zuhaiir <zuhairmehdee@gmail.com>
2025-03-13 22:49:03 -0230

    export default app

--- index.ts
+++ index.ts
@@ -213,11 +213,4 @@ app.get("/game/:gameId/events", async (c) => {
     }
   });
 });
-
-Bun.serve({
-  fetch: app.fetch,
-  port: 3000,
-  hostname: "0.0.0.0",
-});
-
-console.log(`Server running on http://0.0.0.0:${3000}`);
+export default app;
